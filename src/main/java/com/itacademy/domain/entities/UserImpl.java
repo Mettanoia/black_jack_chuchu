284d43c3871802295ef6331805addc0cca8bb127
miguel <miguelsunnata@gmail.com>
2024-08-24 17:37:51 +0200

    Hit and Stand business logic added. Invariants also added.

--- src/main/java/com/itacademy/domain/entities/UserImpl.java
+++ src/main/java/com/itacademy/domain/entities/UserImpl.java
@@ -2,7 +2,10 @@ package com.itacademy.domain.entities;
 
 import lombok.*;
 
+import java.util.OptionalInt;
 import java.util.Set;
+import java.util.function.Predicate;
+import java.util.stream.IntStream;
 
 @Data
 @RequiredArgsConstructor
@@ -21,13 +24,36 @@ final class UserImpl implements User {
     private boolean isStanding = false;
 
     @Override
-    public void hit(Card card){
-        throw new UnsupportedOperationException();
+    public OptionalInt hit(Card card){
+        if (isStanding)
+            throw new IllegalStateException("Cannot call stand when already standing."); // Instead of an Optional.empty because this is a violation of the contract.
+
+        getHand().add(card); // Hand is updated here
+
+        Predicate<Card> isAce = cardInHand -> cardInHand.getRank() != Card.Rank.ACE;
+
+        int totalHandScoreWithoutAces = hand.stream()
+                .filter(isAce.negate())
+                .mapToInt(cardInHand -> cardInHand.getRankValue(false))
+                .sum();
+
+        int totalAces = (int) hand.stream()
+                .filter(isAce)
+                .count();
+
+        return IntStream.range(0, totalAces)
+                .map(i -> totalHandScoreWithoutAces + (i * Card.Rank.ACE.getHighValue() + (totalAces - i) * Card.Rank.ACE.getLowValue()))
+                .filter(i -> i <= 21)
+                .max();
+
     }
 
     @Override
     public void stand(){
-        throw new UnsupportedOperationException();
+        if (isStanding)
+            throw new IllegalStateException("Cannot call stand when already standing.");
+
+        isStanding = true;
     }
 
 }
