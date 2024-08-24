0829ae981ca3013376f54c9c8f76f083b59155da
miguel <miguelsunnata@gmail.com>
2024-08-24 17:37:08 +0200

    HitUseCase signature changed.

--- src/main/java/com/itacademy/games/use_cases/HitUseCase.java
+++ src/main/java/com/itacademy/games/use_cases/HitUseCase.java
@@ -1,8 +1,10 @@
 package com.itacademy.games.use_cases;
 
+import com.itacademy.domain.entities.CardGameState;
+import com.itacademy.domain.entities.User;
 import reactor.core.publisher.Mono;
 
 @FunctionalInterface
 public interface HitUseCase {
-    Mono<Void> exec();
+    Mono<Void> exec(User user, CardGameState cardGameState);
 }
